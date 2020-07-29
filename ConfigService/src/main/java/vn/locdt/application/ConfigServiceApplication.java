d3f0030a7e266bccf1e740b38dace79763a50cb1
dinhtienloc <dinhtienloc1996@gmail.com>
2020-07-30 00:03:45 +0700

    add support docker and docker-compose

--- ConfigService/src/main/java/vn/locdt/application/ConfigServiceApplication.java
+++ ConfigService/src/main/java/vn/locdt/application/ConfigServiceApplication.java
@@ -1,12 +1,10 @@
 package vn.locdt.application;
 
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableRabbit
 @EnableConfigServer
 public class ConfigServiceApplication {
 	
